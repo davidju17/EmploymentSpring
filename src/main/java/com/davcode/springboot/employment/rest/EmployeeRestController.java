c4cef43869a2ba717d77e12487df83cdd6db7237
David Monroy <davidju17@gmail.com>
2025-04-17 16:36:41 +1000

    add put updateEmployee method to EmployeeRestController
    To test this open postman and use put Body,raw, JSON and past the employee in JSON format this will change the employee with the id specified

--- src/main/java/com/davcode/springboot/employment/rest/EmployeeRestController.java
+++ src/main/java/com/davcode/springboot/employment/rest/EmployeeRestController.java
@@ -58,4 +58,10 @@ public class EmployeeRestController
         employee.setId(0);
         return employeeService.save(employee);
     }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee)
+    {
+        return employeeService.save(employee);
+    }
 }
