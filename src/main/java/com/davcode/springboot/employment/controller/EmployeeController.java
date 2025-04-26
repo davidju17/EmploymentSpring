5537824cf66753a3ad9be464a32e842c710a36e6
David Monroy <davidju17@gmail.com>
2025-04-26 11:55:02 +1000

    create saveEmployee metnod
    grab the employee attribute from the employee form action

--- src/main/java/com/davcode/springboot/employment/controller/EmployeeController.java
+++ src/main/java/com/davcode/springboot/employment/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -40,5 +42,13 @@ public class EmployeeController
     {
         model.addAttribute("employee", new Employee());
         return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee)
+    {
+        employeeRepository.save(employee);
+        // use redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
     }
 }
